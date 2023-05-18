8b253637dc51d1ed3adb165b94c1200830afe9dc
Agustin faucetta <agusfau@gmail.com>
2023-05-18 09:41:31 -0300

    nuevo

---
+++ Modelo/Domicilio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Domicilio
+    {
+        public int id {get; set;}
+        public string calle { get; set;}
+        public string altura { get; set;}
+        public string localidad { get; set;}
+        public string provincia { get; set;}
+
+    }
+}
---
+++ Modelo/Perfiles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Contexto;
+
+namespace Modelo
+{
+    public partial class Perfil  
+    {
+       /* public override string ToString()
+        {
+            return this.NOMBRE;
+        }*/
+    }
+}
